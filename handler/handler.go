cec67d66463ec9ebd8c2fcc127f00a79ce0c287c
panutat.p <pp.panutat@gmail.com>
2025-01-24 23:53:39 +0700

    feat: go validator

--- handler/handler.go
+++ handler/handler.go
@@ -4,24 +4,32 @@ import (
 	"log/slog"
 	"net/http"
 
+	govalidator "github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 
 	"boilerplate-go/internal/config"
 	"boilerplate-go/internal/store"
 	"boilerplate-go/internal/usecase"
+	"boilerplate-go/pkg"
 )
 
 type Handler struct {
-	config  *config.Config
-	useCase usecase.IUseCase
-	store   store.IStore
+	config    *config.Config
+	useCase   usecase.IUseCase
+	store     store.IStore
+	validator *govalidator.Validate
 }
 
 func NewHandler(config *config.Config, useCase usecase.IUseCase, store store.IStore) *Handler {
+
+	validator := govalidator.New(govalidator.WithRequiredStructEnabled())
+	validator = pkg.RegisterNullTypes(validator)
+
 	return &Handler{
-		config:  config,
-		useCase: useCase,
-		store:   store,
+		config:    config,
+		useCase:   useCase,
+		store:     store,
+		validator: validator,
 	}
 }
 
