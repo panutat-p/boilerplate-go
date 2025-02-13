254f53024c4f239eee856bbe4b3a06bf0678d0b4
panutat.p <pp.panutat@gmail.com>
2025-02-13 23:59:29 +0700

    feat: graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/caarlos0/env/v11"
 	"github.com/go-playground/validator/v10"
@@ -16,6 +20,10 @@ import (
 	"boilerplate-go/internal/usecase"
 	"boilerplate-go/middleware"
 	"boilerplate-go/pkg"
+)
+
+var (
+	stop = make(chan os.Signal, 1)
 )
 
 func main() {
@@ -67,8 +75,29 @@ func main() {
 	e.GET("/public/reflect", h.GetReflect)
 	e.GET("/public/fruits", h.GetFruits)
 
-	err = e.Start(":" + conf.Port)
-	if err != nil {
-		panic(err)
+	go func() {
+		err := e.Start(":" + conf.Port)
+		if err != nil {
+			slog.Error("Echo server is closed", "reason", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	slog.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		slog.ErrorContext(
+			ctx,
+			"Server forced to shutdown:",
+			slog.Any("error", err),
+		)
 	}
+
+	slog.Info("Server shutdown successfully")
 }
